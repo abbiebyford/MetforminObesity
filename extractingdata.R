67c076c33e77e833294aa5125177eaa613665359
Abbie <abbiebyford@hotmail.co.uk>
2020-07-27 18:19:08 +0100

    started to extract idat files

--- extractingdata.R
+++ extractingdata.R
@@ -4,3 +4,28 @@ Data.file <- "E-MTAB-6418.raw.1.zip"
 download.file(paste(URL,SDRF.file,sep="/"), SDRF.file)
 download.file(paste(URL,Data.file,sep="/"), Data.file)
 unzip(Data.file)
+
+#Read the sdrf file, this is an updated version I added birthweight centiles and LGA/AGA/SGA classes too.
+SDRF <- read.delim("withcentiles.sdrf.txt", check.names=FALSE,stringsAsFactors=FALSE)
+
+#Have a quick look at birthweight classes
+SDRF[,c("Array Data File", "Class")]
+
+#Set the birthweight class as a variable
+Class <- SDRF[, "Class"]
+
+#Set the different levels of class
+levels <- c("LGA", "AGA", "SGA")
+Class <- factor(Class, levels=levels)
+
+str(SDRF)
+
+#Extracting data from idat files
+library(illuminaio)
+#ie. read first file.
+idat <- readIDAT("3998423016_A_Grn.idat")
+names(idat)
+
+#using limma instead
+idatfiles = dir(pattern="idat")
+bgxfile = dir(pattern="bgx") # need to get this from illumina website
